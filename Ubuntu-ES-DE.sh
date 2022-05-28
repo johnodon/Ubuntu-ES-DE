7dcc2ab8842585b6ac34c043c94b9e44bb1e5290
johnodon <johnodon@hotmail.com>
2022-05-28 16:03:20 -0400

    Update Ubuntu-ES-DE.sh

--- Ubuntu-ES-DE.sh
+++ Ubuntu-ES-DE.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/dont-prompt-$USER-for-sudo-password"   # disables prompts for password for sudo
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install openbox obconf git dialog unzip xmlstarlet samba mpv dos2unix mc scrot openssh-server --no-install-recommends -y
+sudo apt install openbox obconf git dialog unzip xmlstarlet samba mpv dos2unix mc scrot openssh-server fuse --no-install-recommends -y
 #sudo apt install build-essential libsdl2-dev libsdl2-gfx-dev libsdl2-ttf-dev libvorbis-dev libsdl2-image-dev autotools-dev     # uncomment this line if you plan to compile hypseus
 wget -O EmulationStation-DE-x64_Current.AppImage  https://gitlab.com/es-de/emulationstation-de/-/package_files/40176633/download
 chmod +x EmulationStation-DE-x64_Current.AppImage
