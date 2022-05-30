e5363c59b981e1dd491d7a626796fd6c86470184
johnodon <johnodon@hotmail.com>
2022-05-30 14:50:34 -0400

    Update Ubuntu-ES-DE.sh

--- Ubuntu-ES-DE.sh
+++ Ubuntu-ES-DE.sh
@@ -29,9 +29,10 @@ fi
 # Menu to present optional packages
 main_menu() {
 PACKAGES=$(dialog --no-tags --clear --backtitle "Main Menu" --title "Optional Packages" \
-    --checklist "This script will install ES-DE and its dependencies even if no optional packages are selected. \
-    Use arrown keys to move up/down and SPACE to select/deselct optional packages. Select OK and press [ENTER] when finished.  \
+    --checklist "Use arrown keys to move up/down and SPACE to select/deselct packages. \
+    Select OK and press [ENTER] when finished. \
     Select CANCEL and press [ENTER] to terminate the script" 30 100 30 \
+    initial_install "Perform initial install (do this only the first time)" on \
     install_intel_driver "Install latest Intel GPU driver" off \
     install_nvidia_driver "Install latest Nvidia GPU driver" off \
     install_mesa "Install latest version of Mesa" off \
@@ -61,7 +62,36 @@ enable_logging() {
 
 
 
-################################################################# START BASE INSTALLATION SECTION ##################################################################
+################################################################## START INSTALLATION SECTION #################################################################
+
+# Readout package selections
+package_selection() {
+    for SELECTION in $PACKAGES; do
+        case $SELECTION in
+        initial_install)
+            initial_install
+            ;;
+        install_intel_driver)
+            install_intel_driver
+            ;;
+        install_nvidia_driver)
+            install_nvidia_driver
+            ;;
+        install_mesa)
+            install_mesa
+            ;;     
+        install_extra_tools)
+            install_extra_tools
+            ;;
+        install_retroarch)
+            install_retroarch
+            ;;
+        install_hypseus_singe)
+            install_hypseus_singe
+            ;;
+     esac
+     done
+ }
 
 # Create file in sudoers.d directory and disable password prompt
 disable_sudo_password() {
@@ -115,38 +145,6 @@ configure_openbox() {
     chown -R $USER:$USER $USER_HOME/.config/openbox
     echo -e "FINISHED configure_openbox \n\n"
 }
-
-################################################################# END BASE INSTALLATION SECTION ##################################################################
-
-
-
-################################################################# START OPTIONAL INSTALLATION SECTION ##################################################################
-
-# Readout package selections
-package_selection() {
-    for SELECTION in $PACKAGES; do
-        case $SELECTION in
-        install_intel_driver)
-            install_intel_driver
-            ;;
-        install_nvidia_driver)
-            install_nvidia_driver
-            ;;
-        install_mesa)
-            install_mesa
-            ;;     
-        install_extra_tools)
-            install_extra_tools
-            ;;
-        install_retroarch)
-            install_retroarch
-            ;;
-        install_hypseus_singe)
-            install_hypseus_singe
-            ;;
-     esac
-     done
- }
 
 # Install latest Intel GPU driver
 install_intel_driver() {
@@ -218,11 +216,10 @@ install_hypseus_singe() {
     echo -e "FINISHED install_hypseus_singe \n\n"
 }
 
-################################################################# END OPTIONAL INSTALLATION SECTION ##################################################################
+################################################################# END PACKAGE SECTION ##################################################################
 
 
-
-################################################################### START CLEANUP SECTION ###################################################################
+################################################################ START CLEANUP SECTION ##################################################################
 
 # Repair any permissions that might have been incorrectly set
 repair_permissions() {
@@ -266,7 +263,7 @@ base_installation() {
 }
 
 ### OPTIONAL Installation Functions ###
-optional_installation() {
+installation() {
     package_selection
 }
 
@@ -278,8 +275,7 @@ cleanup() {
 }
 
 preflight
-base_installation
-optional_installation
+installation
 cleanup
 
 echo "********************************************************************************"
