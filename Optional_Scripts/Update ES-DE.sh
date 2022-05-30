eebf01f6e889540ee8358929ea16ae76f19d136c
johnodon <johnodon@hotmail.com>
2022-05-30 19:40:59 -0400

    Update Update ES-DE.sh

--- Optional_Scripts/Update ES-DE.sh
+++ Optional_Scripts/Update ES-DE.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-if exist ~/EmulationStation-DE-x64_Current.AppImage.old ; then
-    rm ~/EmulationStation-DE-x64_Current.AppImage.old
+if exist /home/$USER/EmulationStation-DE-x64_Current.AppImage.old ; then
+    rm /home/$USER/EmulationStation-DE-x64_Current.AppImage.old
 fi
 mv /home/$USER/EmulationStation-DE-x64_Current.AppImage /home/$USER/EmulationStation-DE-x64_Current.AppImage.old
 wget -O /home/$USER/EmulationStation-DE-x64_Current.AppImage  https://gitlab.com/es-de/emulationstation-de/-/package_files/40176633/download
