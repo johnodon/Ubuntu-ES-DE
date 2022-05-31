cc5937c997d6e89b68fc38461deb94e8525d27b4
johnodon <johnodon@hotmail.com>
2022-05-31 14:44:22 -0400

    Update Update ES-DE.sh

--- Optional_Scripts/Update ES-DE.sh
+++ Optional_Scripts/Update ES-DE.sh
@@ -1,9 +1,16 @@
+
 #! /bin/bash
 
 if exist /home/$USER/EmulationStation-DE-x64_Current.AppImage.old ; then
     rm /home/$USER/EmulationStation-DE-x64_Current.AppImage.old
 fi
+pkill -f -e "/home/$USER/EmulationStation-DE-x64_Current.AppImage --no-splash"
+sleep 2
 mv /home/$USER/EmulationStation-DE-x64_Current.AppImage /home/$USER/EmulationStation-DE-x64_Current.AppImage.old
-wget -O /home/$USER/EmulationStation-DE-x64_Current.AppImage  https://gitlab.com/es-de/emulationstation-de/-/package_files/40176633/download
+wget -O /home/$USER/latest_steam_deck_appimage.txt https://gitlab.com/es-de/emulationstation-de/-/raw/master/es-app/assets/latest_steam_deck_appimage.txt
+APPIMAGELINK=$(cat /home/$USER/latest_steam_deck_appimage.txt | head -3 | tail -1)
+wget -O /home/$USER/EmulationStation-DE-x64_Current.AppImage $APPIMAGELINK
 chown $USER:$USER /home/$USER/EmulationStation-DE-x64_Current.AppImage
 chmod +x /home/$USER/EmulationStation-DE-x64_Current.AppImage
+sleep 2
+/home/$USER/EmulationStation-DE-x64_Current.AppImage --no-splash
