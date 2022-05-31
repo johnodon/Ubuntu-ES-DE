5bc53571ae302094a072587e2e52e9880799d3cf
johnodon <johnodon@hotmail.com>
2022-05-31 19:28:40 -0400

    Update Update ES-DE.sh

--- Optional_Scripts/Update ES-DE.sh
+++ Optional_Scripts/Update ES-DE.sh
@@ -4,7 +4,7 @@
 wget -O /home/$USER/latest_steam_deck_appimage.txt https://gitlab.com/es-de/emulationstation-de/-/raw/master/es-app/assets/latest_steam_deck_appimage.txt
 APPIMAGELINK=$(cat /home/arcade/latest_steam_deck_appimage.txt | head -3 | tail -1)
 [ -f "/home/$USER/EmulationStation-DE-x64_Current.AppImage.old" ] && rm "/home/$USER/EmulationStation-DE-x64_Current.AppImage.old"
-pkill -f -e "/home/$USER/EmulationStation-DE-x64_Current.AppImage --no-splash"
+pkill -f -e "*EmulationStation-DE*"
 [ -f "/home/$USER/EmulationStation-DE-x64_Current.AppImage" ] && mv "/home/$USER/EmulationStation-DE-x64_Current.AppImage" "/home/$USER/EmulationStation-DE-x64_Current.AppImage.old"
 wget -O "/home/$USER/EmulationStation-DE-x64_Current.AppImage" "$APPIMAGELINK" &> ~/output.txt
 chmod +x "/home/$USER/EmulationStation-DE-x64_Current.AppImage"
