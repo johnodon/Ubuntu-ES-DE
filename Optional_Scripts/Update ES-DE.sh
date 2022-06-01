4dcfe6704a309ee34f0850096e9e6a2644b5c3b1
johnodon <johnodon@hotmail.com>
2022-06-01 14:02:36 -0400

    Update Update ES-DE.sh

--- Optional_Scripts/Update ES-DE.sh
+++ Optional_Scripts/Update ES-DE.sh
@@ -1,17 +1,27 @@
+
 #!/bin/bash
 
-wget -O /home/$USER/latest_steam_deck_appimage.txt https://gitlab.com/es-de/emulationstation-de/-/raw/master/es-app/assets/latest_steam_deck_appimage.txt
-APPIMAGELINK=$(cat /home/arcade/latest_steam_deck_appimage.txt | head -3 | tail -1)
+# Declare variables
+APPIMAGELINK=$(curl https://gitlab.com/es-de/emulationstation-de/-/raw/master/es-app/assets/latest_steam_deck_appimage.txt | tail -1)
 ESCOMMAND=$(ps -eo args | grep EmulationStation-DE | head -1)
-echo "$ESCOMMAND"
 APPIMAGEFILE=$(ps -eo args | grep EmulationStation-DE | head -1 | tail -1)
 APPIMAGEFILE2=${APPIMAGEFILE% *}
-echo "$APPIMAGEFILE2"
+
+# Dlete backup of AppImage if exists
 [ -f "$APPIMAGEFILE2.old" ] && rm "$APPIMAGEFILE2.old"
+
+# Kill ES-DE
 pkill -f -e EmulationStatio
+
+# Create backup of AppImage
 [ -f "$APPIMAGEFILE2" ] && mv "$APPIMAGEFILE2" "$APPIMAGEFILE2.old"
-wget -O "$APPIMAGEFILE2" $APPIMAGELINK &> ~/output.txt
+
+# Download latest AppImage and set to executable
+wget -O "$APPIMAGEFILE2" $APPIMAGELINK &> /dev/null
 chmod +x "$APPIMAGEFILE2"
+
+# Start ES-DE
 sleep 2
 $ESCOMMAND &
+
 exit 0
