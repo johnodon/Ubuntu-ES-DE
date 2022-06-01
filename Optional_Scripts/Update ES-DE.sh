3d1f2dc15dfc726eae9e1166889f6a45c2dfb5f2
johnodon <johnodon@hotmail.com>
2022-06-01 11:59:13 -0400

    Update Update ES-DE.sh

--- Optional_Scripts/Update ES-DE.sh
+++ Optional_Scripts/Update ES-DE.sh
@@ -1,12 +1,17 @@
-
 #!/bin/bash
 
 wget -O /home/$USER/latest_steam_deck_appimage.txt https://gitlab.com/es-de/emulationstation-de/-/raw/master/es-app/assets/latest_steam_deck_appimage.txt
 APPIMAGELINK=$(cat /home/arcade/latest_steam_deck_appimage.txt | head -3 | tail -1)
-[ -f "/home/$USER/EmulationStation-DE-x64_Current.AppImage.old" ] && rm "/home/$USER/EmulationStation-DE-x64_Current.AppImage.old"
-pkill -f -e "*EmulationStation-DE*"
-[ -f "/home/$USER/EmulationStation-DE-x64_Current.AppImage" ] && mv "/home/$USER/EmulationStation-DE-x64_Current.AppImage" "/home/$USER/EmulationStation-DE-x64_Current.AppImage.old"
-wget -O "/home/$USER/EmulationStation-DE-x64_Current.AppImage" "$APPIMAGELINK" &> ~/output.txt
-chmod +x "/home/$USER/EmulationStation-DE-x64_Current.AppImage"
+ESCOMMAND=$(ps -eo args | grep EmulationStation-DE | head -1)
+echo "$ESCOMMAND"
+APPIMAGEFILE=$(ps -eo args | grep EmulationStation-DE | head -1 | tail -1)
+APPIMAGEFILE2=${APPIMAGEFILE% *}
+echo "$APPIMAGEFILE2"
+[ -f "$APPIMAGEFILE2.old" ] && rm "$APPIMAGEFILE2.old"
+pkill -f -e EmulationStatio
+[ -f "$APPIMAGEFILE2" ] && mv "$APPIMAGEFILE2" "$APPIMAGEFILE2.old"
+wget -O "$APPIMAGEFILE2" $APPIMAGELINK &> ~/output.txt
+chmod +x "$APPIMAGEFILE2"
 sleep 2
-/home/$USER/EmulationStation-DE-x64_Current.AppImage --no-splash
+$ESCOMMAND &
+exit 0
